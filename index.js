e718dd2126c4c4dd4ec256053dcbc2c353146b27
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-05 20:27:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser="Yana"
+
+let welcomeMessage="Welcome to Flatbook, " +currentUser+"!"
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, '
+// let shortGreeting="Welcome, "+currentUser.charAt(0) +"!"
